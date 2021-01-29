d16c6b7d3480fe1d8701f79043507dc11b2b0ace
Gulshirin Berdiyeva <gulshirinberdiyeva01@gmail.com>
2021-01-29 17:54:23 +0300

    IMPLEMENTED: RegExTextHandler class one more method

--- src/main/java/com/epam/task/second/logic/RegExTextHandler.java
+++ src/main/java/com/epam/task/second/logic/RegExTextHandler.java
@@ -3,6 +3,7 @@ package com.epam.task.second.logic;
 import com.epam.task.second.entity.Text;
 import org.apache.log4j.Logger;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,18 +11,38 @@ public class RegExTextHandler implements ITextHandler{
 
     private final static Logger LOGGER = Logger.getLogger(RegExTextHandler.class);
 
-    public String splitTextIntoWordsAndSpaces(Text text) {
+    private Matcher splitIntoWords(Text text) {
         String copyText = text.toString();
-        Pattern pattern = Pattern.compile("[^\\p{Punct}&&\\D]");
+        Pattern pattern = Pattern.compile("\\w+[^\\p{Punct}]|\\s");
         Matcher matcher = pattern.matcher(copyText);
+        return matcher;
+    }
+
+    public String splitTextIntoWordsAndSpaces(Text text) {
+        Matcher matcher = splitIntoWords(text);
         StringBuilder buffer = new StringBuilder();
 
         while (matcher.find()){
-            int start = matcher.start();
-            int end = matcher.end();
-            buffer.append(copyText.substring(start, end));
+            buffer.append(matcher.group());
         }
         LOGGER.info("Text splitted successfully by regex");
+        String result = buffer.toString();
+        return result.trim();
+    }
+
+    public String replaceAllWords(Text text, int length, String replacement) {
+        String wordsTemp = splitTextIntoWordsAndSpaces(text);
+        String[]  words = wordsTemp.split("\\s");
+        StringBuilder buffer = new StringBuilder();
+
+        for (String word : words){
+            if (word.length() == length){
+                buffer.append(replacement + " ");
+                LOGGER.info(word + " replaced");
+            }else {
+                buffer.append(word + " ");
+            }
+        }
         String result = buffer.toString();
         return result.trim();
     }
@@ -34,9 +55,7 @@ public class RegExTextHandler implements ITextHandler{
         return null;
     }
 
-    public String replaceAllWords(Text text, int length, String replacement) {
-        return null;
-    }
+
 
     public String removeAllWordsStartingWithConsonant(Text text, int length) {
         return null;
