d4f25adb575d79b257a50ce604cd8576d0dc7564
Gulshirin Berdiyeva <gulshirinberdiyeva01@gmail.com>
2021-02-01 03:19:09 +0300

    ADDED: TxtFileReader class read method and Tests for it

--- src/main/java/com/epam/task/third/data/TxtFileReader.java
+++ src/main/java/com/epam/task/third/data/TxtFileReader.java
@@ -1,9 +1,29 @@
 package com.epam.task.third.data;
 
+import org.apache.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TxtFileReader {
+    private final static Logger LOGGER = Logger.getLogger(TxtFileReader.class);
 
-    public void read(String fileName)throws DataException{
-
+    public List<String> read(String fileName) throws DataException{
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            List<String> data = new ArrayList<String>();
+            while (reader.ready()){
+                data.add(reader.readLine());
+            }
+            LOGGER.info("Data read successfully");
+            return data;
+        } catch (IOException e) {
+            LOGGER.fatal(e.getMessage(), e);
+            throw new DataException(e.getMessage(), e);
+        }
     }
 
 }
--- src/test/java/com/epam/task/third/data/TxtFileReaderTest.java
+++ src/test/java/com/epam/task/third/data/TxtFileReaderTest.java
@@ -1,7 +1,47 @@
 package com.epam.task.third.data;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class TxtFileReaderTest {
+    private TxtFileReader reader = new TxtFileReader();
+    private final String FILE_NAME = "src/test/resources/input.txt";
+    private List<String> expected = Arrays.asList("5.0 3.0 0.0 5.0",
+                                                  "2.n -4 4",
+                                                  "6 2.5 0 ?",
+                                                  "-2.0 -1.0 1.0 -5.0");
+    private final String WRONG_FILE_NAME = "src/test/resources/input";
+    private boolean expectedException = true;
 
+    @Test
+    public void testReadShouldReadDataWhenFileNameApplied() throws DataException{
+        //when
+        List<String> actual = reader.read(FILE_NAME);
 
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testReadShouldThrowExceptionWhenWrongFileNameApplied(){
+        //given
+        boolean actualException;
+
+        //when
+        try {
+            List<String> actual = reader.read(WRONG_FILE_NAME);
+             actualException = false;
+            Assert.assertEquals(expectedException, actualException);
+        }
+
+        //then
+        catch (DataException e) {
+            actualException = true;
+            Assert.assertEquals(expectedException, actualException);
+        }
+    }
 
 }
