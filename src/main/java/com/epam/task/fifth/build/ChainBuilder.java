e6284687712340e3b29d84fbdb221716d24f91d4
Gulshirin Berdiyeva <gulshirinberdiyeva01@gmail.com>
2021-02-23 00:38:20 +0300

    ADDED: ChainBuilder class

---
+++ src/main/java/com/epam/task/fifth/build/ChainBuilder.java
@@ -0,0 +1,14 @@
+package com.epam.task.fifth.build;
+
+import com.epam.task.fifth.parser.ParagraphsParser;
+import com.epam.task.fifth.parser.Parser;
+import com.epam.task.fifth.parser.SentencesParser;
+import com.epam.task.fifth.parser.TextParser;
+
+public class ChainBuilder {
+
+    public Parser build(){
+        return new TextParser(new ParagraphsParser(new SentencesParser(null)));
+    }
+
+}
